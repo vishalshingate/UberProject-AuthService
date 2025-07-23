931fb65541ba0d92e5776f1e56ac536aaddba615
vishal shingate <vshingate@ptc.com>
2025-07-23 20:24:43 +0530

    Added JWT token creation method

---
+++ src/main/java/com/example/uberprojectauthservice/services/JwtService.java
@@ -0,0 +1,48 @@
+package com.example.uberprojectauthservice.services;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService implements CommandLineRunner {
+
+    @Value("${jwt.expiry}")
+    private int expiry;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    // this method creates the brand new jwt token based on the payload
+    private String createToken(Map<String, Object> payload, String username) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expiry*1000L);
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        return Jwts.builder()
+            .claims(payload).issuedAt(new Date(System.currentTimeMillis())).expiration(expiryDate).subject(username)
+            .signWith(key)
+            .compact();
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Map<String, Object> payload = new HashMap<String, Object>();
+        payload.put("username", "admin");
+        payload.put("password", "admin");
+        payload.put("role", "USER");
+        payload.put("enabled", true);
+        String token = createToken(payload, "admin");
+        System.out.println(token);
+    }
+}
